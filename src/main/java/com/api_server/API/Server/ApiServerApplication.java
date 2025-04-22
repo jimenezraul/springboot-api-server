28d1ddb26f3b86b3bb3532d7d5985b62c16037e0
Raul <jimenezraul1981@gmail.com>
2025-04-22 16:02:02 -0400

    Initial commit

---
+++ src/main/java/com/api_server/API/Server/ApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.api_server.API.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}
